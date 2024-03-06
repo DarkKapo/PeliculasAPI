259adcc5e435d822f8d588492daecaae5ae1f631
DarkKapo <rodrigo.qc@hotmail.es>
2024-03-06 13:53:59 -0300

    Agrego migracion de Pelicula

---
+++ PeliculasAPI/Controllers/Entidades/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Controllers.Entidades
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 300, ErrorMessage = "El máximo de letras es {1}")]
+        public string Titulo { get; set; }
+        public bool EnCines { get; set; }
+        public DateTime FechaEstreno{ get; set; }
+        [StringLength(maximumLength: 2100, ErrorMessage = "El máximo de letras es {1}")]
+        public string Poster { get; set; }
+    }
+}
