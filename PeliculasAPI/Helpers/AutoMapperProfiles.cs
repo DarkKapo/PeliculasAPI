5af12bf1d5087373d85f505cd32afa6962285f7e
Rodrigo Quezada Candia <rquezada@amf.cl>
2024-03-15 11:55:53 -0300

    Agrego el include para que muestre los generos y actores en una pelicula

--- PeliculasAPI/Helpers/AutoMapperProfiles.cs
+++ PeliculasAPI/Helpers/AutoMapperProfiles.cs
@@ -18,7 +18,26 @@ namespace PeliculasAPI.Helpers
                 .ForMember(x => x.Poster, options => options.Ignore())
                 .ForMember(x => x.PeliculasGeneros, options => options.MapFrom(MapPeliculasGeneros)) //Agrega los datos a la tabla intermedia
                 .ForMember(x => x.PeliculasActores, options => options.MapFrom(MapPeliculasActores));//Agrega los datos a la tabla intermedia
+            CreateMap<Pelicula, PeliculaDetallesDTO>()
+                .ForMember(x => x.Generos, options => options.MapFrom(MapPeliculasGeneros))
+                .ForMember(x => x.Actores, options => options.MapFrom(MapPeliculasActores));
             CreateMap<PeliculaPatchDTO, Pelicula>().ReverseMap();
+        }
+
+        private List<GeneroDTO> MapPeliculasGeneros(Pelicula pelicula, PeliculaDetallesDTO peliculaDetallesDTO)
+        {
+            var resultado = new List<GeneroDTO>();
+            if(pelicula.PeliculasGeneros == null) return resultado;
+            foreach(var genero in pelicula.PeliculasGeneros) resultado.Add(new GeneroDTO() { Id = genero.GeneroId, Nombre = genero.Genero.Nombre });
+            return resultado;
+        }
+
+        private List<ActorPeliculaDetalleDTO> MapPeliculasActores(Pelicula pelicula, PeliculaDetallesDTO peliculaDetallesDTO)
+        {
+            var resultado = new List<ActorPeliculaDetalleDTO>();
+            if(pelicula.PeliculasActores == null) return resultado;
+            foreach(var actor in pelicula.PeliculasActores) resultado.Add(new ActorPeliculaDetalleDTO() { ActorId = actor.ActorId, Personaje = actor.Personaje, NombrePersona = actor.Actor.Nombre });
+            return resultado;
         }
         //La idea es asignar los ids de generos y actores a pelicula (parecid a asignar un id a una FK)
         private List<PeliculasGeneros> MapPeliculasGeneros(PeliculaCreacionDTO peliculaCreacionDTO, Pelicula pelicula)
