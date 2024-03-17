e91c40e93e5a9c1c14964dfb59a08bb3d434313d
Rodrigo Quezada Candia <rquezada@amf.cl>
2024-03-17 17:47:59 -0300

    Agrego filtro por cercanía de las salas de cine

--- PeliculasAPI/Controllers/SalasDeCineController.cs
+++ PeliculasAPI/Controllers/SalasDeCineController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
 using PeliculasAPI.Controllers.Entidades;
 using PeliculasAPI.DTOs;
 
@@ -7,14 +9,17 @@ namespace PeliculasAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SalasDeCineController: CustomBaseController
+    public class SalasDeCineController : CustomBaseController
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
-        public SalasDeCineController(ApplicationDbContext context, IMapper mapper): base(context, mapper)
+        private readonly GeometryFactory geometryFactory;
+
+        public SalasDeCineController(ApplicationDbContext context, IMapper mapper, GeometryFactory geometryFactory) : base(context, mapper)
         {
             this.context = context;
-            this.mapper = mapper;   
+            this.mapper = mapper;
+            this.geometryFactory = geometryFactory;
         }
 
         [HttpGet]
@@ -27,6 +32,25 @@ namespace PeliculasAPI.Controllers
         public async Task<ActionResult<SalaDeCineDTO>> Get(int id)
         {
             return await Get<SalaDeCine, SalaDeCineDTO>(id);
+        }
+
+        [HttpGet("Cercanos")]
+        public async Task<ActionResult<List<SalaDeCineCercanoDTO>>> Cercanos([FromQuery] SalaDeCineCercanoFiltroDTO salaDeCineCercanoFiltroDTO)
+        {
+            var userLocation = geometryFactory.CreatePoint(new Coordinate(salaDeCineCercanoFiltroDTO.Longitud, salaDeCineCercanoFiltroDTO.Latitud));
+            var salasDeCine = await context.SalasDeCine
+                .OrderBy(x => x.Ubicacion.Distance(userLocation))
+                .Where(x => x.Ubicacion.IsWithinDistance(userLocation, salaDeCineCercanoFiltroDTO.DistanciaEnKms * 1000))
+                .Select(x => new SalaDeCineCercanoDTO
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre,
+                    Latitud = x.Ubicacion.Y,
+                    Longitud = x.Ubicacion.X,
+                    DistanciaEnMetros = Math.Round(x.Ubicacion.Distance(userLocation))
+                }).ToListAsync();
+
+            return salasDeCine;
         }
 
         [HttpPost]
---
+++ PeliculasAPI/Migrations/20240317192451_AddSeedSalaDeCine.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace PeliculasAPI.Migrations
+{
+    public partial class AddSeedSalaDeCine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SalasDeCine",
+                columns: new[] { "Id", "Nombre", "Ubicacion" },
+                values: new object[] { 5, "Vespucio", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-33.51554 -70.58515)") });
+
+            migrationBuilder.InsertData(
+                table: "SalasDeCine",
+                columns: new[] { "Id", "Nombre", "Ubicacion" },
+                values: new object[] { 6, "Village East Cinema", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-73.986227 40.730898)") });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SalasDeCine",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "SalasDeCine",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
